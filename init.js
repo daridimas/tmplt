8c1a6c263cacbfdb1c244a533cce4cddec5ef64c
daridimas <inboxdimas@gmail.com>
2022-06-12 11:24:27 +0700

    change title with params from ecma script.

--- init.js
+++ init.js
@@ -1,5 +1,9 @@
-// function untuk ganti title web DOM
-function changeTitle(params) {
-  console.log("our parameter = ", params)
+// // function untuk ganti title web DOM
+// function changeTitle(params) {
+//   console.log("our parameter = ", params)
+//   document.title = params
+// }
+
+const changeTitle = (params) => {
   document.title = params
 }
