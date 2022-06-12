e24194902f060fddc1ef399afa05b4f02b7fc593
daridimas <inboxdimas@gmail.com>
2022-06-12 11:30:31 +0700

    ubah jadi form register dan init di clear

--- init.js
+++
@@ -1,9 +0,0 @@
-// // function untuk ganti title web DOM
-// function changeTitle(params) {
-//   console.log("our parameter = ", params)
-//   document.title = params
-// }
-
-const changeTitle = (params) => {
-  document.title = params
-}
