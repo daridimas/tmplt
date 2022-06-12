a1e83ab5db5cf9a249374be00dc47c082bdd2144
daridimas <inboxdimas@gmail.com>
2022-06-12 11:16:30 +0700

    master file

---
+++ init.js
@@ -0,0 +1,8 @@
+const title = "deacourse"
+console.log(title)
+// function untuk ganti title web DOM
+function changeTitle() {
+  console.log("function siap di gas")
+  console.log(document.title)
+  document.title = "My Games EAEAEA"
+}
