e5686c76d6eb1e4aca75284d31017dcd687f5f67
daridimas <inboxdimas@gmail.com>
2022-06-12 12:07:43 +0700

    create session token

--- init.js
+++ init.js
@@ -1,6 +1,9 @@
 // Declaring element
 const username = document.getElementById("username")
 
+onload = function () {}
+
 function register() {
   console.log(username.value)
+  sessionStorage.setItem("token", username.value)
 }
