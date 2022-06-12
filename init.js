ee1a9cd9d4a5d26e7c8aba21ad91b2a135c31a04
daridimas <inboxdimas@gmail.com>
2022-06-12 13:08:13 +0700

    get set item

--- init.js
+++ init.js
@@ -1,7 +1,11 @@
 // Declaring element
 const username = document.getElementById("username")
 
-onload = function () {}
+onload = function () {
+  console.log("test onload")
+  const token = this.sessionStorage.getItem("token")
+  console.log(token)
+}
 
 function register() {
   console.log(username.value)
