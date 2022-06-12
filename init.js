9eeebd0f1880da72f85550362f0aabccad0e95ab
daridimas <inboxdimas@gmail.com>
2022-06-12 13:17:04 +0700

    kalo udah register dan simpan session, maka formnya hilang

--- init.js
+++ init.js
@@ -1,10 +1,12 @@
 // Declaring element
 const username = document.getElementById("username")
+const registerForm = document.getElementById("registerForm")
 
 onload = function () {
   console.log("test onload")
   const token = this.sessionStorage.getItem("token")
   console.log(token)
+  registerForm.style.display = "none"
 }
 
 function register() {
